8fdd493ceb8cb9a27764f47fc29f7fd9124453f1
Biely <whxtyy@foxmail.com>
2024-02-22 18:53:45 +0800

    fix

--- localLife/certificate/prepare.go
+++ localLife/certificate/prepare.go
@@ -19,39 +19,61 @@ type CertPrepareRequest struct {
 }
 
 type Amount struct {
-	CouponPayAmount  int `json:"coupon_pay_amount"`
-	ListMarketAmount int `json:"list_market_amount"`
-	OriginalAmount   int `json:"original_amount"`
-	PayAmount        int `json:"pay_amount"`
+	CouponPayAmount  int32 `json:"coupon_pay_amount"`
+	ListMarketAmount int32 `json:"list_market_amount"`
+	OriginalAmount   int32 `json:"original_amount"`
+	PayAmount        int32 `json:"pay_amount"`
+}
+
+type TimeCardAmount struct {
+	OriginalAmount         int32 `json:"original_amount"`
+	PayAmount              int32 `json:"pay_amount"`
+	MerchantTicketAmount   int32 `json:"merchant_ticket_amount"`
+	ListMarketAmount       int32 `json:"list_market_amount"`
+	PlatformDiscountAmount int32 `json:"platform_discount_amount"`
+	PaymentDiscountAmount  int32 `json:"payment_discount_amount"`
+	CouponPayAmount        int32 `json:"coupon_pay_amount"`
+}
+
+type SerialAmountList struct {
+	SerialNumb int32          `json:"serial_numb"`
+	Amount     TimeCardAmount `json:"amount"`
 }
 type Sku struct {
-	MarketPrice   int    `json:"market_price"`
+	MarketPrice   int32  `json:"market_price"`
 	SkuID         string `json:"sku_id"`
-	SoldStartTime int    `json:"sold_start_time"`
+	SoldStartTime int32  `json:"sold_start_time"`
 	ThirdSkuID    string `json:"third_sku_id"`
 	Title         string `json:"title"`
 	AccountID     string `json:"account_id"`
-	GrouponType   int    `json:"groupon_type"`
+	GrouponType   int32  `json:"groupon_type"`
 }
 type Certificates struct {
-	StartTime     int    `json:"start_time"`
+	StartTime     int64  `json:"start_time"`
 	Amount        Amount `json:"amount"`
 	CertificateID int64  `json:"certificate_id"`
 	EncryptedCode string `json:"encrypted_code"`
-	ExpireTime    int    `json:"expire_time"`
+	ExpireTime    int64  `json:"expire_time"`
 	Sku           Sku    `json:"sku"`
 }
 type CertificatesV2 struct {
-	StartTime     int    `json:"start_time"`
+	StartTime     int64  `json:"start_time"`
 	Amount        Amount `json:"amount"`
 	CertificateID int64  `json:"certificate_id"`
 	EncryptedCode string `json:"encrypted_code"`
-	ExpireTime    int    `json:"expire_time"`
+	ExpireTime    int64  `json:"expire_time"`
 	Sku           Sku    `json:"sku"`
+}
+
+type TimeCard struct {
+	TimesCount       int32              `json:"times_count"`
+	TimesUsed        int32              `json:"times_used"`
+	SerialAmountList []SerialAmountList `json:"serial_amount_list"`
 }
 type PrepareData struct {
 	Certificates   []Certificates   `json:"certificates"`
 	CertificatesV2 []CertificatesV2 `json:"certificates_v2"`
+	TimeCard       TimeCard         `json:"time_card"`
 	OrderID        string           `json:"order_id"`
 	VerifyToken    string           `json:"verify_token"`
 	ErrorCode      int              `json:"error_code"`
