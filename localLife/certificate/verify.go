8c517c5d2bf6900d0c96d20ecc8eadc3e9c00f1e
Biely <whxtyy@foxmail.com>
2024-03-02 16:04:57 +0800

    fix

--- localLife/certificate/verify.go
+++ localLife/certificate/verify.go
@@ -1,15 +1,23 @@
 package certificate
 
-import "github.com/Biely/douyinSDK/util"
+import (
+	"fmt"
+
+	"github.com/Biely/douyinSDK/response"
+	"github.com/Biely/douyinSDK/util"
+)
 
 const (
 	certificateVerifyUrl = "https://open.douyin.com/goodlife/v1/fulfilment/certificate/verify/"
 )
 
 type CertVerifyRequest struct {
+	VerifyToken    string   `json:"verify_token" form:"verify_token"`
+	EncryptedCodes []string `json:"encrypted_codes" form:"encrypted_codes[]"`
+	PoiID          string   `json:"poi_id" form:"poi_id"`
 }
 
-func (certificate *Certificate) CertificateVerify(in *CertVerifyRequest) (res interface{}, err error) {
+func (certificate *Certificate) CertificateVerify(in *CertVerifyRequest) (rep interface{}, err error) {
 	accessToken, err := certificate.GetAccessToken()
 	// fmt.Println(accessToken)
 	if err != nil {
@@ -18,9 +26,15 @@ func (certificate *Certificate) CertificateVerify(in *CertVerifyRequest) (res in
 	header := map[string]string{
 		"access-token": accessToken,
 	}
-	res, err = util.PostJSON(certificateVerifyUrl, in, header)
+	res, err := util.PostJSON(certificateVerifyUrl, in, header)
 	if err != nil {
 		return nil, err
+	}
+	fmt.Println(string(res))
+	rep = response.Response{}
+	err = util.DecodeWithError(res, &rep, "CertVerify")
+	if err != nil {
+		return nil, fmt.Errorf("decodeWithError is invalid %v", err)
 	}
 	return
 }
